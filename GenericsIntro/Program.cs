3f23be35a9fc09b464ce646bcbc9634855b0a165
Fatma DURAK <86356645+fatmadurak@users.noreply.github.com>
2021-06-27 19:57:17 +0300

    Add files via upload

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+         
+
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("Fatma");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Usame");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+     
+
+        }
+    }
+}
