013f790d69ee94d2e0fbf730cbb1be31b235c650
Fatma DURAK <86356645+fatmadurak@users.noreply.github.com>
2021-06-27 19:53:51 +0300

    Add files via upload

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+  public class MusteriManager
+    {
+
+        public void Ekleme(Musteri musteri) {
+
+
+            Console.WriteLine(musteri.Ad+"   "+musteri.Soyad+ "  Eklendi.");
+        
+        }
+
+
+        public void Silme(Musteri musteri) 
+        {
+
+            Console.WriteLine(musteri.Ad + "  " + musteri.Soyad + "  Silindi.");
+        }
+
+
+        public void Listeleme(Musteri[] musteriler) 
+        {
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad+"  "+musteri.Soyad+"   "+musteri.TcNo);
+
+            }
+
+            Console.WriteLine(" Listelendi..");
+          
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Id = 1;
+            musteri1.Ad = "Fatma";
+            musteri1.Soyad = "DURAK";
+            musteri1.TcNo = "3246586";
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Id = 2;
+            musteri2.Ad = "Usame";
+            musteri2.Soyad = "DURAK";
+            musteri2.TcNo = "15646486";
+
+            Musteri musteri3 = new Musteri();
+
+            musteri3.Id = 3;
+            musteri3.Ad = "Zeliha";
+            musteri3.Soyad = "YILANCI";
+            musteri3.TcNo = "648651";
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1,musteri2,musteri3};
+
+            musteriManager.Ekleme(musteri1);
+            musteriManager.Silme(musteri2);
+            musteriManager.Listeleme(musteriler);
+
+        }
+    }
+}
