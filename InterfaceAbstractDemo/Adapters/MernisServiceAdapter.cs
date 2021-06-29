4bff43e7af68aad04b4bbb8ea975133f46a74579
Fatma DURAK <86356645+fatmadurak@users.noreply.github.com>
2021-06-29 15:46:24 +0300

    Add files via upload

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+            var result = client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName, customer.LastName, customer.DateOfBirth.Year).Result;
+
+            bool check = result.Body.TCKimlikNoDogrulaResult;
+            return check;
+        }
+    }
+}
