b0de712a55e0c0c8f850199d6118600c6cfe688d
Fatma DURAK <86356645+fatmadurak@users.noreply.github.com>
2021-07-21 13:10:04 +0300

    Add files via upload

---
+++ Events/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+
+    public delegate void StockKontrol();
+   public class Product
+    {
+        public int _stock;
+
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+
+        public event StockKontrol StockKontrolEvent;
+        public string ProductName { get; set; }
+        public int Stock { get 
+            {
+                return _stock;
+            }
+            set 
+            {
+                _stock = value;
+
+                if (value<=15 && StockKontrolEvent!=null)
+                {
+                    StockKontrolEvent();
+                }
+            }
+        }
+
+
+        public void Sell(int amount) 
+        
+        {
+
+            Stock -= amount;
+            Console.WriteLine("{1} Stock amount : {0}",Stock,ProductName);
+        
+        }
+
+
+
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product hardDisk = new Product(50);
+            hardDisk.ProductName = "HardDisk";
+
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockKontrolEvent += Gsm_StockKontrolEvent;
+
+            for (int i = 0; i < 10; i++)
+            {
+                hardDisk.Sell(10);
+                gsm.Sell(10);
+
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static void Gsm_StockKontrolEvent()
+        {
+            Console.WriteLine("Gsm about to finish");
+        }
+    }
+}
